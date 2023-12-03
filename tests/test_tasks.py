291f295b380fd1ce69bf5e63b949781df4fa9439
Michael Eliachevitch <m.eliachevitch@posteo.de>
2023-12-03 18:32:15 +0100

    Add test for download task

---
+++ tests/test_tasks.py
@@ -0,0 +1,22 @@
+import tempfile
+from pathlib import Path
+from unittest.mock import Mock, patch
+
+from yellow_taxis.tasks.download import DownloadTask
+
+
+@patch("yellow_taxis.fetch.download_monthly_data")
+def test_download_task_with_expected_args(mock_download_monthly_data: Mock):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        download_task = DownloadTask(year=2009, month=9, result_dir=tmpdirname)
+        expected_result_path = (
+            Path(tmpdirname) / "2009" / "09" / download_task.result_path.name
+        )
+        download_task.run()
+        mock_download_monthly_data.assert_called_once_with(
+            2009,
+            9,
+            file_name=expected_result_path,
+            make_directories=True,
+            overwrite=False,
+        )
