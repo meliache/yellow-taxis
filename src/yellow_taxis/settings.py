667717465a9de9421ab888a91c015ff0bf3324a2
Michael Eliachevitch <m.eliachevitch@posteo.de>
2023-12-07 12:50:01 +0100

    Move from tomli to built-in tomllib
    tomllib is builtin from Python 3.11

--- src/yellow_taxis/settings.py
+++ src/yellow_taxis/settings.py
@@ -1,8 +1,8 @@
-# TODO implement some settings system to set result directory etc.
+"""Module to read the settings from the ``config.toml`` file."""
+
+import tomllib
 from pathlib import Path
 from typing import Any
-
-import tomli
 
 REPO_ROOT = (Path(__file__).parent.parent.parent).absolute()
 SETTINGS_FILE_PATH = REPO_ROOT / "config.toml"
@@ -11,11 +11,12 @@ SETTINGS_FILE_PATH = REPO_ROOT / "config.toml"
 def get_settings() -> dict[str, Any]:
     """Get settings dictionary from ``config.toml`` file in repo root."""
     with open(SETTINGS_FILE_PATH, "rb") as settings_file:
-        return tomli.load(settings_file)
+        return tomllib.load(settings_file)
 
 
 def get_result_dir() -> Path | None:
-    """Return default ``result_dir`` from settings if provided or ``None`` otherwise."""
+    """Return default ``result_dir`` from settings if provided or ``None``
+    otherwise."""
     result_dir: str | None = get_settings().get("result_dir")
     if result_dir:
         if not isinstance(result_dir, str):
